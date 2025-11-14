0a91ad859f20b779ea8ab293da37d275cafe10fd
samutaitto <samu.taitto@gmail.com>
2025-11-14 14:11:07 +0200

    Tehty projekti

---
+++ src/App.java
@@ -0,0 +1,56 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Auto a1 = new Auto();
+        
+       a1.naytaTiedot();
+       a1.kiihdyta();
+       a1.jarruta();
+       a1.pysahtyy();
+
+        
+    }
+}
+
+class Auto {
+
+   // Ominaisuudet
+    int bensanMaara;
+    String merkki;
+    String malli;
+
+    // Muodostimet
+    
+    // Parametriton muodostin tai non-parameter
+    public Auto() {
+        merkki = "";
+        malli = "";
+        bensanMaara = 10;  
+    }
+
+    // Täysparametrinen muodostin
+    public Auto(String m, String v, int l) {
+        merkki = m;
+        malli = v;
+        bensanMaara = l; 
+    } 
+
+    public void jarruta() {
+        System.out.println("Auto jarruttaa");
+        
+    }
+
+    public void pysahtyy() {
+        System.out.println("Auto pysähtyi");
+    }
+
+    public void kiihdyta() {
+        System.out.println("Auto kiihtyy");
+        
+    }
+
+    public void naytaTiedot() {
+        System.out.println("Merkki: " + merkki);
+        System.out.println("Malli: " + malli);
+        System.out.println("Bensan määrä: " + bensanMaara + "l");
+    }
+}
